304f201015910743351baf99b67235a17ce2c92f
nihbertolo <nickbertolo@gmail.com>
2025-11-16 12:37:31 -0300

    feat(Project): include rest/soap ports

--- src/main/java/br/com/nicolebertolo/Main.java
+++ src/main/java/br/com/nicolebertolo/Main.java
@@ -1,6 +1,7 @@
 package br.com.nicolebertolo;
 
 import br.com.nicolebertolo.infrastructure.adapter.inbound.grpc.ListingGrpc;
+import br.com.nicolebertolo.proto.listing.ListingServiceGrpc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
@@ -13,21 +14,25 @@ import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws Exception {
+
         ApplicationContext context = SpringApplication.run(Main.class, args);
-        ListingGrpc listingGrpc = context.getBean(ListingGrpc.class);
+
+        // pega a implementação correta anotada como @Service no Spring
+        ListingServiceGrpc.ListingServiceImplBase listingService =
+                context.getBean(ListingGrpc.class);
 
         Server server = ServerBuilder
                 .forPort(50051)
-                .addService(listingGrpc)
+                .addService(listingService) // <-- agora está correto
                 .addService(ProtoReflectionService.newInstance())
                 .executor(Executors.newVirtualThreadPerTaskExecutor())
                 .build()
                 .start();
 
         System.out.println("gRPC Server iniciado na porta 50051");
-        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
 
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
         server.awaitTermination();
     }
 }
